1948588f05d926d99b0d5c057ce652520cdd0a68
jwijffels <jwijffels@bnosac.be>
2016-08-17 16:47:12 +0200

    lack of default value of svalid gave unexpected warnings

--- R/k.R
+++ R/k.R
@@ -3,5 +3,5 @@ klist <- function() .Call(C_klist)
 kinit <- function(user=Sys.getenv("USER"), realm=getOption("kerberos.realm"),
                   cache=NULL, password=NULL, keytab=if(file.exists("~/.keytab")) "~/.keytab" else NULL) {
     principal <- if (is.null(user) || is.null(realm)) NULL else paste(user, realm, sep='@')
-    .Call(C_kinit, cache, principal, password, if (is.null(keytab)) NULL else path.expand(keytab))
+    .Call(C_kinit, cache, principal, password, if (is.null(keytab)) NULL else path.expand(keytab), 0L)
 }
